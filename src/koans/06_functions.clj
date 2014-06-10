3039cc4dbe403712aae568aa08521e3388c6d8bf
Emory Merryman <emory.merryman@gmail.com>
2014-06-10 15:30:48 -0400

    I spent forever on this one

--- src/koans/06_functions.clj
+++ src/koans/06_functions.clj
@@ -33,7 +33,7 @@
            #(* %1 %2)))
 
   "Higher-order functions take function arguments"
-  (= 25 (___
+  (= 25 ( (fn[f](f 5))
           (fn [n] (* n n))))
 
   "But they are often better written using the names of functions"
