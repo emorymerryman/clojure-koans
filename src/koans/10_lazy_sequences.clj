05e2c752665e8293fba7ef9ef369b976df260873
Emory Merryman <emory.merryman@gmail.com>
2014-06-10 20:14:08 -0400

    had to cheat but the concept was easy

--- src/koans/10_lazy_sequences.clj
+++ src/koans/10_lazy_sequences.clj
@@ -10,11 +10,11 @@
 
   "Only take what you need when the sequence is large"
   (= [0 1 2 3 4 5 6 7 8 9]
-     (take __ (range 100)))
+     (take 10 (range 100)))
 
   "Or limit results by dropping what you don't need"
   (= [95 96 97 98 99]
-     (drop __ (range 100)))
+     (drop 95 (range 100)))
 
   "Iteration provides an infinite lazy sequence"
   (= __ (take 20 (iterate inc 0)))
