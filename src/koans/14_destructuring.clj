f5c23cd649ae8094a19dab8aa8867839eaa618c8
Emory Merryman <emory.merryman@gmail.com>
2014-06-11 14:05:00 -0400

    not sure what to do with aliases

--- src/koans/14_destructuring.clj
+++ src/koans/14_destructuring.clj
@@ -22,7 +22,7 @@
   (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
-       __))
+       str(first-name " " last-name " aka " ) ))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
