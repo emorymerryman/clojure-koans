7da71d522fa0b97b79a306d623c7bf8b0ce3f08e
Emory Merryman <emory.merryman@gmail.com>
2014-06-11 17:34:48 -0400

    I don't like refs because they are mutable which I thought was a dirty word

--- src/koans/15_refs.clj
+++ src/koans/15_refs.clj
@@ -9,12 +9,15 @@
   (= "hello" (deref the-world))
 
   "You can get the word more succinctly, but it's the same"
-  (= __ @the-world)
+  (= "hello" @the-world)
 
   "You can be the change you wish to see in the world."
-  (= __ (do
+  (= "better" (do
           (dosync (ref-set the-world "better"))
           @the-world))
+
+  "You can get the word more succinctly, but it's the same"
+  (= "better" @the-world)
 
   "Alter where you need not replace"
   (= __ (let [exclamator (fn [x] (str x "!"))]
