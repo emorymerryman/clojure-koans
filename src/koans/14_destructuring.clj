14d43516fda3661735e088b9fb2fed67c931cdba
Emory Merryman <emory.merryman@gmail.com>
2014-06-11 16:59:42 -0400

    I cheated and I had some formatting issues

--- src/koans/14_destructuring.clj
+++ src/koans/14_destructuring.clj
@@ -29,7 +29,8 @@
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+       { :original-parts full-name
+       	 :named-parts {:first first-name :last last-name}}))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
